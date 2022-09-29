aabc9543cea5005ccbfd451828fd8379444e6fed
GolubevAndrey <golubev_andrei87@mail.ru>
2022-09-30 00:36:14 +0300

    выполненная задача

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -1,8 +1,11 @@
-public class Bicycle {
-    public String modelName;
-    public int wheelsCount;
-
-    public void updateTyre() {
-        System.out.println("Меняем покрышку");
+public class Bicycle extends Vehicle {
+    public Bicycle(String modelName, int wheelsCount) {
+        super(modelName,wheelsCount);
+    }
+    @Override
+    public void checkEngine() {
+    }
+    @Override
+    public void checkTrailer() {
     }
 }
